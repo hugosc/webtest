2b5c5c986f5701a7547c697f88714ea2205b8221
Yeastiest <hugo.serranochan@gmail.com>
2025-05-18 03:42:35 +0100

    initial framework

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+      <div style="font-family: sans-serif; text-align: center;">
+      <h1>{ "Hello from Yew!" }</h1>
+      <p>{ "This is a super simple Yew app." }</p>
+      </div>
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    yew::Renderer::<App>::new().render();
+}
